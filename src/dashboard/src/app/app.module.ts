00f9230f8a51dca794e6ef8ee3e8bfee2fffc7ad
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2020-01-16 20:10:49 +0800

    Move copy existing layer panel

--- src/dashboard/src/app/app.module.ts
+++ src/dashboard/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { FormsModule } from '@angular/forms';
 import { CreateLayerPanelComponent } from './project-manager-widget/create-layer-panel/create-layer-panel.component';
 import { ProjectManagerHomePanelComponent } from './project-manager-widget/project-manager-home-panel/project-manager-home-panel.component';
 import { ImportDataPanelComponent } from './project-manager-widget/create-layer-panel/import-data-panel/import-data-panel.component';
-import { CopyExistingLayerPanelComponent } from './project-manager-widget/copy-existing-layer-panel/copy-existing-layer-panel.component'
+import { CopyExistingLayerPanelComponent } from './project-manager-widget/create-layer-panel/copy-existing-layer-panel/copy-existing-layer-panel.component'
 
 @NgModule({
   declarations: [

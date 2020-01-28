9ab31c0b050ce3e5fbcce9dd64cf4701a371c2f9
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2020-01-29 00:40:44 +0800

    proceed styles

--- src/dashboard/src/app/side-menu/side-menu-section/proceed-inner-button/proceed-inner-button.component.ts
+++ src/dashboard/src/app/side-menu/side-menu-section/proceed-inner-button/proceed-inner-button.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProceedInnerButtonComponent implements OnInit {
 
   @Input() title: string;
+  @Input() outerClass: string;
   @Input() iconClass: string;
 
   constructor() { }
